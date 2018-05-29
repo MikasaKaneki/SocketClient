625578eef283e84ced917fca730405c519c5f332
zhangziqing <mojinyang8@outlook.com>
2018-05-29 19:49:23 +0800

    update client Register

--- Assets/Scripts/UIPanel/RoomListPanel.cs
+++ Assets/Scripts/UIPanel/RoomListPanel.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomListPanel : BasePanel
 {
     private RectTransform _leftTransform;
     private RectTransform _rightTransform;
+    private Button _closeBtn;
 
     public override void OnEnter()
     {
@@ -14,6 +17,12 @@ public class RoomListPanel : BasePanel
         gameObject.SetActive(true);
         _leftTransform = transform.Find("BattleRes").GetComponent<RectTransform>();
         _rightTransform = transform.Find("RoomList").GetComponent<RectTransform>();
+        _closeBtn=transform.Find("RoomList/")
+        _closeBtn.onClick.AddListener(() =>
+        {
+            QuitAnim();
+            _uiManager.PopPanel();
+        });
         EnterAnim();
     }
 
@@ -21,31 +30,36 @@ public class RoomListPanel : BasePanel
     public override void OnExit()
     {
         base.OnExit();
+        QuitAnim();
     }
 
     public override void OnPause()
     {
         base.OnPause();
+        QuitAnim();
     }
 
     public override void OnResume()
     {
         base.OnResume();
+        EnterAnim();
     }
 
 
     private void EnterAnim()
     {
         _leftTransform.localPosition = new Vector3(-1000, 0, 0);
-        _leftTransform.DOMoveX(-400, 0.5f);
+        _leftTransform.DOLocalMoveX(-400, 0.5f);
+
 
         _rightTransform.localPosition = new Vector3(1000, 0, 0);
-        _leftTransform.DOMoveX(144, 0.5f);
+        _rightTransform.DOLocalMoveX(144, 0.5f);
     }
 
 
     private void QuitAnim()
     {
-        _leftTransform.DOMoveX(-400, 0.5f);
+        _leftTransform.DOMoveX(-1000, 0.5f);
+        _rightTransform.DOMoveX(1000, 0.5f).OnComplete(() => { gameObject.SetActive(false); });
     }
 }
